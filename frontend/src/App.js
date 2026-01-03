56f6004349e28df2a28eef3de47fe3e3619dd9f2
vedhanth <vedhanthmanju@gmail.com>
2026-01-03 15:46:14 +0530

    Analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 
 // Custom hook for scroll animations
 const useIntersectionObserver = () => {
